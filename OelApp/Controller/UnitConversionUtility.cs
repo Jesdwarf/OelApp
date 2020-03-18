ae797eb8248f9c01a9728a89135c6758d37503c8
Jesdwarf <hemermads@gmail.com>
2020-03-18 13:43:48 +0100

    Tekst

--- OelApp/Controller/UnitConversionUtility.cs
+++ OelApp/Controller/UnitConversionUtility.cs
@@ -3,6 +3,12 @@
     public class UnitConversionUtility
     {
         public int UnitConversion()
+        //For kvinder:
+        //Alkohol i gram / (kropsvægten i kg x 60 %) - (0,15 x timer fra drikkestart) = promille
+        //For mænd:
+        //Alkohol i gram / (kropsvægten i kg x 70 %) - (0,15 x timer fra drikkestart) = promille
+        //Du finder oversigt over forskellige drikkevarers indhold af alkohol i gram i artiklen fakta om alkohol
+
         {
             throw new System.NotImplementedException();
         }
